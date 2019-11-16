565de0cb504874f6223f2a11509fdb7563d2f992
Aquarian-Age <xiaoyaoke7630@gmail.com>
2019-11-16 17:20:28 +0800

    调整了代码结构 添加了起课问询

--- src/info.rs
+++ src/info.rs
@@ -1,40 +1,66 @@
-pub mod info;
-use crate::info::list_info;
+//卜法卷可卜算内容列表
 
-//　六宫的结构体
-struct LiuGong {
-    yigong: String,
-    ergong: String,
-    sangong: String,
-    sigong: String,
-    wugong: String,
-    liugong: String,
+use std::io;
+#[allow(dead_code)]
+enum Info {
+    //    占求财,
+    //    占行人,
+    //    占寻人,
+    //    占家先,
+    //    占失物,
+    //    占病人,
+    //    占走失,
+    //    占访贵,
+    //    占合伙,
+    //    占婚姻,
+    //    占埋葬,
+    Zhanxiufang,
+    //    占送方,
+    //    占预兆,
+    //    占官非,
+    //    占风水,
+    //    占虚实,
+    Zhanqita,
 }
+//列出可占卜信息
+pub fn list_info() -> u32 {
+    println!(
+        "道家小六壬卜法卷
+        测算内容：
+        1:占求财 2:占行人 3:占寻人 
+        4:占家先 5:占失物 6:占病人 
+        7:占走失 8:占访贵 9:占合伙 
+        10:占婚姻 11:占埋葬 12:占修方
+        13:占送方 14:占预兆 15:占官非 
+        16:占风水 17:占虚实 18:占其他"
+    );
+    println!("根据数字选择要测算的内容");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).ok().expect("输入错误...");
+    let _n: u32 = input.trim().parse::<u32>().unwrap();
+    //println!("选择{}", _n);
 
-//　打印宫位
-fn gong_wei() {
-    let gong_wei = LiuGong {
-        yigong: String::from("大安"),
-        ergong: String::from("留连"),
-        sangong: String::from("速喜"),
-        sigong: String::from("赤口"),
-        wugong: String::from("小吉"),
-        liugong: String::from("空亡"),
-    };
-
-    println!("####################");
-    println!("   宫位信息列表");
-    println! {"   # {}: 一宫", gong_wei.yigong};
-    println!("   # {}: 二宫", gong_wei.ergong);
-    println!("   # {}: 三宫", gong_wei.sangong);
-    println!("   # {}: 四宫", gong_wei.sigong);
-    println!("   # {}: 五宫", gong_wei.wugong);
-    println!("   # {}: 六宫", gong_wei.liugong);
+    valur_in_info(_n);
+    return _n;
 }
-
-/*
+//根据输入遍历占卜信息
+pub fn valur_in_info(n: u32) {
+    let mut _n1 = n;
+    //println!("n in value = {}", _n1);
+    if _n1 == 1 {
+        println!("选择{}: 占求才", _n1);
+        println!(" ");
+        // get_input(); //这里导致重复获取用户输入
+        qi_ke();
+    }
+    if _n1 == 18 {
+        println!("选择{}: 占其他", _n1);
+        //get_input();
+        qi_ke();
+    }
+}
 //获取用户输入
-fn get_input() -> (u32, u32, u32) {
+pub fn get_input() -> (u32, u32, u32) {
     println!("输入十以内的任意三个数字");
     println!("如果安农历时间起课　输入农历的月　日　时　时：十二时辰");
 
@@ -53,51 +79,11 @@ fn get_input() -> (u32, u32, u32) {
     let _three: u32 = inputthree.trim().parse::<u32>().unwrap();
     return (_one, _two, _three);
 }
-*/
 
-//定宫
-//数字为落宫位置　根据数字定宫位：
-//１大安; ２留连;　３速喜; 4赤口; 5小吉; ０空亡;
-//同一个宫位循环数字
-//大安　１  7(6*1+1)  13(6*2+1) 19(6*3+1)  25(6*4+1)  31(6*5+1) 大安括号内都加一
-//留连　２　8(6*1+2)  14(6*2+2)  20(6*3+2)  26(6*4+2) 32(6*5+2) 留连括号内都加２
-//速喜　３　9(6*1+3)  15(6*2+3)  21(6*3+3)  27(6*4+3) 33(6*5+3) 速喜括号内都加三
-//赤口  4  10(6*1+4)  16(6*2+4)  22(6*3+4) 28(6*4+4)  34(6*5+4) 赤口括号内都加４
-//小吉  5  11(6*1+5)  17(6*2+5)  23(6*3+5) 29(6*4+5)  35(6*5+5) 小吉括号内都加５
-//空亡  6  12(6*1+6)  18(6*2+6)  24(6*3+6) 30(6*4+6)  36(6*5+6) 空亡括号内都加6
-/*
-fn ding_gong() {
+//根据输入起课
+pub fn qi_ke() {
     let (one, two, three) = get_input();
-    //定一宫
-    let yigong: u32 = one % 6;
-    println!("数字{}落{}宫", one, yigong);
-    //定二 三宫
-    if two > 6 {
-        let ergong: u32 = yigong + two - 7;
-        println!("数字{}落{}宫", two, ergong);
-        let sangong: u32 = ergong + three - 7;
-        println!("数字{}落{}宫", three, sangong);
-    } else if two <= 6 {
-        let ergong: u32 = yigong + two - 1;
-        println!("数字{}落{}宫", two, ergong);
-        let sangong: u32 = ergong + three - 1;
-        println!("数字{}落{}宫", three, sangong);
-    }
-}
-*/
-/*
-fn qike() {
-    let (one, two, three) = get_input();
-    /*
-    let yigong: Wrapping<u32> = Wrapping::<u32>(one) % Wrapping(6);
-    let ergong: Wrapping<u32> = yigong + Wrapping(two) - Wrapping(7);
-    let sangong: Wrapping<u32> = (ergong) + Wrapping(three) - Wrapping(1);
 
-    println!("数字{}落{}宫", one, yigong);
-    println!("数字{}落{}宫", two, ergong);
-    println!("数字{}落{}宫", three, sangong);
-    println!("依据以上数字起课为:");
-    */
     println!("依据以上数字起课为:");
     //根据确定落宫数字来定宫
     //定一宫
@@ -160,28 +146,4 @@ fn qike() {
             _ => println!("三宫什么鬼?"),
         }
     }
-}
-*/
-fn main() {
-    list_info();
-    //起课前的询问
-    //    let args: Vec<String> = env::args().collect();
-    //    let question = Question::new(&args).unwrap_or_else(|err| {
-    //        println!("问题参数解析：{}", err);
-    //
-    //        process::exit(1);
-    //    });
-
-    //    if let Err(e) = wen(question) {
-    //        println!("Application error: {}", e);
-    //        process::exit(1);
-    //    }
-
-    //调用宫位
-    gong_wei();
-    //调用　获取用户输入
-    // get_input();
-    //定宫
-    //ding_gong();
-    // qike();
 }
